897eb85100b120659c622c95a9bb53c8ce9c04b2
SOgz12Z3Ce <sogz12z3ce@gmail.com>
2025-08-19 02:50:23 +0800

    feat(Sephiroth): add Sephirah class to pack Sephiroth

---
+++ src/Sephiroth.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+
+namespace LobotomyCorpSaveManager.Sephiroth
+{
+	public class Sephirah
+	{
+		private enum SephirahType
+		{
+			Malkuth,
+			Yesod,
+			Hod,
+			Netzach,
+			Tiphereth,
+			Gebura,
+			Chesed,
+			Daat,
+			Binah,
+			Hokma,
+			Kether,
+		}
+
+		private readonly SephirahType type;
+
+		private Sephirah(SephirahType type)
+		{
+			this.type = type;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is Sephirah))
+			{
+				return false;
+			}
+			var s = obj as Sephirah;
+			return s.type == this.type;
+		}
+
+		public override int GetHashCode()
+		{
+			return this.type.GetHashCode();
+		}
+
+		public static readonly Sephirah Malkuth = new Sephirah(SephirahType.Malkuth);
+		public static readonly Sephirah Yesod = new Sephirah(SephirahType.Yesod);
+		public static readonly Sephirah Hod = new Sephirah(SephirahType.Hod);
+		public static readonly Sephirah Netzach = new Sephirah(SephirahType.Netzach);
+		public static readonly Sephirah Tiphereth = new Sephirah(SephirahType.Tiphereth);
+		public static readonly Sephirah Gebura = new Sephirah(SephirahType.Gebura);
+		public static readonly Sephirah Chesed = new Sephirah(SephirahType.Chesed);
+		public static readonly Sephirah Daat = new Sephirah(SephirahType.Daat);
+		public static readonly Sephirah Binah = new Sephirah(SephirahType.Binah);
+		public static readonly Sephirah Hokma = new Sephirah(SephirahType.Hokma);
+		public static readonly Sephirah Kether = new Sephirah(SephirahType.Kether);
+		public static readonly List<Sephirah> All = new List<Sephirah>()
+		{
+			Sephirah.Malkuth,
+			Sephirah.Yesod,
+			Sephirah.Hod,
+			Sephirah.Netzach,
+			Sephirah.Tiphereth,
+			Sephirah.Gebura,
+			Sephirah.Chesed,
+			Sephirah.Daat,
+			Sephirah.Binah,
+			Sephirah.Hokma,
+			Sephirah.Kether,
+		};
+		public static readonly List<Sephirah> AllWithoutDaat = new List<Sephirah>()
+		{
+			Sephirah.Malkuth,
+			Sephirah.Yesod,
+			Sephirah.Hod,
+			Sephirah.Netzach,
+			Sephirah.Tiphereth,
+			Sephirah.Gebura,
+			Sephirah.Chesed,
+			Sephirah.Binah,
+			Sephirah.Hokma,
+			Sephirah.Kether,
+		};
+	}
+}
